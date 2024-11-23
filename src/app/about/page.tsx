d0cb87ce02f3f3070b167b1ab9e14ce62bdb460d
ghazaleaghaei <ghazale.ag@gmail.com>
2024-11-23 17:01:59 +0330

    commited by script

---
+++ src/app/about/page.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import {
+    AppstoreOutlined,
+    ContainerOutlined,
+    DesktopOutlined,
+    MailOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+    { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
+    { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
+    { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
+    {
+        key: 'sub1',
+        label: 'Navigation One',
+        icon: <MailOutlined />,
+        children: [
+            { key: '5', label: 'Option 5' },
+            { key: '6', label: 'Option 6' },
+            { key: '7', label: 'Option 7' },
+            { key: '8', label: 'Option 8' },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+];
+
+const About: React.FC = () => {
+
+    const [data, setData] = useState<string>("")
+    const [collapsed, setCollapsed] = useState(true);
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
+
+    useEffect(() => {
+        const storedColor = localStorage.getItem("color");
+        const color: string = storedColor ? JSON.parse(storedColor) : "bg-blue-700";
+        setData(color)
+    }, [])
+
+    return (
+        <div>
+            <header className={`${data} w-full p-2 h-fit`}>
+                <Button
+                    onClick={toggleCollapsed}
+                    className="mb-10"
+                >
+                    {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                </Button>
+            </header>
+            <Menu
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                theme="dark"
+                inlineCollapsed={collapsed}
+                items={items}
+            />
+        </div>
+    )
+}
+export default About
---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+
+import { Button } from "antd";
+
+export default function Home() {
+    const items = [
+        {
+            name: "blue",
+            backgroundColor: "bg-blue-300"
+        },
+        {
+            name: "red",
+            backgroundColor: "bg-red-300"
+        },
+        {
+            name: "yellow",
+            backgroundColor: "bg-yellow-300"
+        },
+    ]
+
+    const handleNavigate = (data: string) => {
+        localStorage.setItem("color", JSON.stringify(data))
+    }
+    return (
+        <section className="">
+            <h1>home</h1>
+            <form className="w-full flex flex-col max-w-sm mx-auto border rounded gap-2 p-8">
+                {
+                    items.map((item, index) => <Button
+                        type="button"
+                        onClick={() => handleNavigate(item.backgroundColor)}
+                        className={`${item.backgroundColor} w-32 min-w-fit mx-auto`}
+                        key={index}
+                        href="/about"
+                    >
+                        {item.name}
+                    </Button>)
+                }
+            </form>
+        </section>
+    );
+}
